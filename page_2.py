d1deb00117d79890ef22058f091d6d06e96d7c66
louismaughan <58695294+louismaughan@users.noreply.github.com>
2025-04-21 19:55:35 +0100

    Add files via upload

--- page_2.py
+++ page_2.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 st.header("Apps and wins 2025")
 st.sidebar.markdown("Apps and wins")
-st.markdown("Who has made the most appearances this year and who has the best win percentage.")
+st.markdown("Who has made the most appearances this year, who has the best win percentage and who has won the most defensive player of the match awards.")
 st.markdown("Win percentage only counts players who've played more than 5 games.")
 
 df = pd.read_csv('lpf.csv')
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
    st.subheader("Win percentage")
@@ -22,4 +22,11 @@ with col2:
    app = df[['Player','Apps']]
    app = app.sort_values('Apps',ascending=False).head(10)[['Player','Apps']]
    st.write(app)
+
+with col3:   
+   st.subheader("DPOTM")
+   dpotm = df[['Player','DPOTM']]
+   dpotm = dpotm.sort_values('DPOTM',ascending=False).head(5)[['Player','DPOTM']]
+   st.write(dpotm)
+
 
