0cbe7cf63886ae25ca66c2556de5652458674c44
louismaughan <58695294+louismaughan@users.noreply.github.com>
2025-05-24 08:39:32 +0100

    Add files via upload

--- main_page.py
+++ main_page.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Wood Street Bears FC")
+st.title("Lloyd Park Old Boys CFC")
 
 st.markdown("Every Sunday at 9am in Lloyd Park")
 st.header("Current tournament champions")
@@ -9,7 +9,13 @@ st.markdown("Dav's yellow peril (Feb 2025): Dav, Steve, Felipe, Liam, Alwyn, Joe
 st.markdown("Player of the tournament - Steve")
 st.markdown("Top scorer - Louis")
 
+st.header("Results")
+st.markdown("Sat 10 May 2025 - LPOB CFC 2 - 0 Coppermill Swifts Vets")
+st.markdown("Goals: Alec 2")
+st.markdown("Assists: Stu, James")
+
 st.header("Fixtures")
-st.markdown("Sat 10 May 2025 - Wood Street Bears v Coppermill Swifts Vets")
+st.markdown("Sun 8 June 2025 - LPOB CFC v Gary Old men")
+st.markdown("Sat 5 July 2025 - Summer tournament (Peter May)")
 
 
