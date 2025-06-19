bc8ae5297d37b45884ee2fea922c93616ac85747
louismaughan <58695294+louismaughan@users.noreply.github.com>
2025-06-19 08:00:20 +0100

    Add files via upload

--- main_page.py
+++ main_page.py
@@ -10,11 +10,12 @@ st.markdown("Player of the tournament - Steve")
 st.markdown("Top scorer - Louis")
 
 st.header("Results")
-st.markdown("Sat 10 May 2025 - LPOB CFC 2 - 0 Coppermill Swifts Vets")
-st.markdown("Goals: Alec 2")
-st.markdown("Assists: Stu, James")
+st.markdown("Sun 8 June 2025 - LPOB CFC 4 - 3 Gary Oldmen")
+st.markdown("Goals: Alex, Stu 3")
+st.markdown("Assists: Louis 3, James")
 
 st.header("Fixtures")
 st.markdown("Sat 5 July 2025 - Summer tournament (Peter May)")
+
 
 
