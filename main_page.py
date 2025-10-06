7ca4f6521183eed3dd8c93749a7822301e377432
louismaughan <58695294+louismaughan@users.noreply.github.com>
2025-10-06 10:27:43 +0100

    Add files via upload

--- main_page.py
+++ main_page.py
@@ -5,19 +5,18 @@ st.title("Lloyd Park Old Boys CFC")
 
 st.markdown("Every Sunday at 9am in Lloyd Park")
 st.header("Current tournament champions")
-st.markdown("Matthews' Blues (July 2025): Matthews, Steve, Donnie, Stu D, Ben M, Pete, Felipe, Vince, Chris")
-st.markdown("Player of the tournament - Donnie")
+st.markdown("Riz's Rockets (Sept 2025): Riz, Matthews, Dav, Colin, Will, Adam S, Stu D, Joe C")
+st.markdown("Player of the tournament - Ben M")
 
 
 st.header("Results")
-st.markdown("Sat 23 August 2025 - Clapton CFC vets 3 - 3 LPOB")
-st.markdown("Goals: Stu 2, Gregg")
-st.markdown("Assists: Stu, Louis, Riz")
+st.markdown("Sun 21 Sept 2025 - Coppermill Swifts 1 - 1 LPOB")
+st.markdown("Goals: Stu")
+st.markdown("Assists: Joe")
 
 st.header("Fixtures")
-st.markdown("Sat 13 September - Tournament 5")
-st.markdown("Sun 21 September v Coppermill Swifts")
 st.markdown("Sat 25 October v Bury Rangers (A) (VFA Cup)")
+
 
 
 
