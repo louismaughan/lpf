70ce30f8abcfdd083c6a2b7f8fd2bdb5b2e1fa51
louismaughan <58695294+louismaughan@users.noreply.github.com>
2026-04-05 08:13:55 +0100

    Add files via upload

--- main_page.py
+++ main_page.py
@@ -4,18 +4,10 @@ import pandas as pd
 st.title("Lloyd Park Old Boys CFC")
 
 st.markdown("Every Sunday at 9am in Lloyd Park")
-st.header("Current tournament champions")
-st.markdown("Riz's Rockets (Sept 2025): Riz, Matthews, Dav, Colin, Will, Adam S, Stu D, Joe C")
-st.markdown("Player of the tournament - Ben M")
 
-
-st.header("Results")
-st.markdown("Sun 21 Sept 2025 - Coppermill Swifts 1 - 1 LPOB")
-st.markdown("Goals: Stu")
-st.markdown("Assists: Joe")
-
-st.header("Fixtures")
-st.markdown("Sat 25 October v Bury Rangers (A) (VFA Cup)")
+st.header("Dates")
+st.markdown("Sun 12 April v City of London vets (A) (Friendly)")
+st.markdown("Sat 25 April Tournament")
 
 
 
