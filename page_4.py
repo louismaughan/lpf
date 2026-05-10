df0669f7ed57b6029bf6ee3fb2540af6c46dd5a8
louismaughan <58695294+louismaughan@users.noreply.github.com>
2026-05-10 13:57:17 +0100

    Add files via upload

--- page_4.py
+++ page_4.py
@@ -19,11 +19,11 @@ with col2:
    st.subheader("Goals against per game")
    ga = df[['Player','Apps','GA/g']]
    ga90 = ga[ga['Apps'] > 4]
-   ga90 = ga90.sort_values('GA/g',ascending=False).head(10)[['Player','GA/g']]
+   ga90 = ga90.sort_values('GA/g',ascending=True).head(10)[['Player','GA/g']]
    st.write(ga90)
 
 with col3:   
-   st.subheader("GD per game")
+   st.subheader("Goal difference per game")
    gd = df[['Player','Apps','GD']]
    gd90 = gd[gd['Apps'] > 4]
    gd90 = gd90.sort_values('GD',ascending=False).head(10)[['Player','GD']]
